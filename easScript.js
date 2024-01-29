2de01aca86c402530e9482b1a83b01c63e61f910
WheatSeeds <georg23kuz@gmail.com>
2024-01-29 20:55:50 +0600

    Version 0.0.3 / Added color selection

--- easScript.js
+++ easScript.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector('#grid');
 
+const sizeArray = [8,16,32,64,128]
+
 let defautColor = '#000000';
 let eraserC = '#FFFFFF'
 let color = defautColor;
@@ -8,27 +10,23 @@ const brush = document.querySelector('#brush');
 const eraser = document.querySelector('#eraser');
 const fill = document.querySelector('#fill');
 const clean = document.querySelector('#clean');
+const colorSelection = document.querySelector('#colorSelection');
+
 
 brush.onclick = () => color = defautColor;
 eraser.onclick = () => color = eraserC;
 fill.onclick = () => fillGrid();
 clean.onclick = () => clearGrid();
+colorSelection.oninput = (e) => changeColor(e.target.value);
 
+function changeColor(value){
+    color = value;
+}
 
 let keyTracker = false;
 document.onmousedown = () => keyTracker = true;
 document.onmouseup = () => keyTracker = false;
-for (let i = 0; i < 256; i++) {
-    const grid_content = document.createElement('div');
-    grid_content.classList.add('content');
-    grid_content.setAttribute('id','content')
-    grid_content.onmouseover = () => {
-        if (keyTracker === true) {
-            grid_content.style.backgroundColor = color;
-        }
-    }
-    grid.append(grid_content);
-}
+
 
 function clearGrid() {
     document.querySelectorAll('#content').forEach(function(e){
@@ -37,6 +35,20 @@ function clearGrid() {
 }
 function fillGrid() {
     document.querySelectorAll('#content').forEach(function(e){
-        e.style.background = defautColor;
+        e.style.background = color;
     })
+}
+
+for (let i = 0; i < 256; i++) {
+    const grid_content = document.createElement('div');
+    grid_content.classList.add('content');
+    grid_content.setAttribute('id', 'content')
+    grid_content.style.width = `32px`;
+    grid_content.style.height = `32px`;
+    grid_content.onmouseover = () => {
+        if (keyTracker === true) {
+            grid_content.style.backgroundColor = color;
+        }
+    }
+    grid.append(grid_content);
 }
